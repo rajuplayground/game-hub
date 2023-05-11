5090bb58c557c829b43984cda9c04c6c51070cdb
Raju George <test@example.com>
2023-05-11 16:35:31 +0530

    initial completed with dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import logo from "./assets/logo.webp";
 import bullsEye from "./assets/bulls-eye.webp";
 import thumbsUp from "./assets/thumbs-up.webp";
 import gameIcon from "./assets/game-icon-ph.jpg";
-import { TbSearch } from "react-icons/tb";
 import { BsChevronDown } from "react-icons/bs";
 import { Menu, Transition } from "@headlessui/react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [platform, setPlatform] = useState("");
 
   return (
     <div>
@@ -19,7 +19,7 @@ function App() {
         </div>
         <div className="flex-1">
           <form>
-            <label for="simple-search" className="sr-only">
+            <label htmlFor="simple-search" className="sr-only">
               Search
             </label>
             <div className="relative w-full">
@@ -28,13 +28,13 @@ function App() {
                   aria-hidden="true"
                   className="w-5 h-5 text-neutral-500 dark:text-neutral-200"
                   fill="currentColor"
-                  viewbox="0 0 20 20"
+                  viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </div>
@@ -67,101 +67,141 @@ function App() {
         <div className="flex-1">
           <h1 className="font-bold text-5xl dark:text-white">Games</h1>
           <div className="h-[20px]"></div>
-          <div>
-            <Menu as="div" className="relative inline-block text-left">
-              <div>
-                <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                  Options
-                  <BsChevronDown
-                    className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
-                    aria-hidden="true"
-                  />
-                </Menu.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <div className="px-1 py-1 ">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-violet-500 text-white"
-                              : "text-gray-900"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          Edit
-                        </button>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-violet-500 text-white"
-                              : "text-gray-900"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          Duplicate
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </div>
-                  <div className="px-1 py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-violet-500 text-white"
-                              : "text-gray-900"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          Archive
-                        </button>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-violet-500 text-white"
-                              : "text-gray-900"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          Move
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </div>
-                  <div className="px-1 py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-violet-500 text-white"
-                              : "text-gray-900"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          Delete
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </div>
-                </Menu.Items>
-              </Transition>
-            </Menu>
+          <div className="relative flex gap-5">
+            <div className="flex gap-4">
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <Menu.Button className="flex items-center rounded-md bg-neutral-200 dark:bg-neutral-800 px-4 py-2 text-md fon t-medium text-neutral-700 dark:text-neutral-300 hover:bg-neutral-300 dark:hover:bg-neutral-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                    Platforms
+                    <BsChevronDown
+                      className="ml-2 -mr-1 h-5 w-5 text-neutral-800 dark:text-neutral-300"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute w-56 left-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-neutral-900 shadow-lg dark:border dark:border-neutral-600 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? "bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400"
+                                : "text-neutral-900 dark:text-neutral-300"
+                            } w-full text-left rounded-md px-2 py-2 text-md`}
+                            onClick={() => setPlatform("PC")}
+                          >
+                            PC
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? "bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400"
+                                : "text-neutral-900 dark:text-neutral-300"
+                            } w-full text-left rounded-md px-2 py-2 text-md`}
+                          >
+                            Commodore / Amigo
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? "bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400"
+                                : "text-neutral-900 dark:text-neutral-300"
+                            } w-full text-left rounded-md px-2 py-2 text-md`}
+                          >
+                            Playstation
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <Menu.Button className="flex items-center rounded-md bg-neutral-200 dark:bg-neutral-800 px-4 py-2 text-md fon t-medium text-neutral-700 dark:text-neutral-300 hover:bg-neutral-300 dark:hover:bg-neutral-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                    Order by: Relevance
+                    <BsChevronDown
+                      className="ml-2 -mr-1 h-5 w-5 text-neutral-800 dark:text-neutral-300"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute w-56 left-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-neutral-900 shadow-lg dark:border dark:border-neutral-600 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? "bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400"
+                                : "text-neutral-900 dark:text-neutral-300"
+                            } w-full text-left rounded-md px-2 py-2 text-md`}
+                            onClick={() => {
+                              setPlatform("PC");
+                            }}
+                          >
+                            PC
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? "bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400"
+                                : "text-neutral-900 dark:text-neutral-300"
+                            } w-full text-left rounded-md px-2 py-2 text-md`}
+                          >
+                            Commodore / Amigo
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? "bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400"
+                                : "text-neutral-900 dark:text-neutral-300"
+                            } w-full text-left rounded-md px-2 py-2 text-md`}
+                          >
+                            Playstation
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
           </div>
           <div className="h-[20px]"></div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
@@ -173,7 +213,7 @@ function App() {
                     <svg
                       stroke="currentColor"
                       fill="currentColor"
-                      stroke-width="0"
+                      strokeWidth="0"
                       viewBox="0 0 448 512"
                       focusable="false"
                       className="text-neutral-500"
@@ -186,7 +226,7 @@ function App() {
                     <svg
                       stroke="currentColor"
                       fill="currentColor"
-                      stroke-width="0"
+                      strokeWidth="0"
                       viewBox="0 0 576 512"
                       focusable="false"
                       className="text-neutral-500"
@@ -199,7 +239,7 @@ function App() {
                     <svg
                       stroke="currentColor"
                       fill="currentColor"
-                      stroke-width="0"
+                      strokeWidth="0"
                       viewBox="0 0 512 512"
                       focusable="false"
                       className="text-neutral-500"
@@ -212,7 +252,7 @@ function App() {
                     <svg
                       stroke="currentColor"
                       fill="currentColor"
-                      stroke-width="0"
+                      strokeWidth="0"
                       role="img"
                       viewBox="0 0 24 24"
                       focusable="false"
